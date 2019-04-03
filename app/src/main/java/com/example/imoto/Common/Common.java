8e9e5136a12705bdce481ddfd8b46512cb92e1b0
Miroslav Resutik <miroslav2.resutik@live.uwe.ac.uk>
2019-04-03 20:15:02 +0100

    Weather function Created Fragment display system Get Location of device in latitude and longitude Build Retrofit request to OpenWeatherMap Design part of user interface for weather fragment {Sourcetree}

---
+++ app/src/main/java/com/example/imoto/Common/Common.java
@@ -0,0 +1,9 @@
+package com.example.imoto.Common;
+
+import android.location.Location;
+
+public class Common {
+
+    public static final String Weather_API = "0ec6d0deb6ac1907a613c8489fdd48dc";
+    public static Location current_location = null;
+}
